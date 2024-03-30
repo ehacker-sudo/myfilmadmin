1e29cbc1ed41df87d7a0564ae4ecccfc23f3b844
Thanh <tta424516@gmail.com>
2024-03-30 22:33:07 +0700

    Login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -19,31 +19,36 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $user = User::where('email', $request->email)->first();
-        if ($user != null) {
-            if (Hash::check($request->password, $user->password)) {
-
-                $tokenResult = $user->createToken('Personal Access Token', ['server:update']);
-                return response()->json([
-                    'result' => true,
-                    'message' => 'Đã đăng nhập thành công',
-                    'status' => 2,
-                    'access_token' => $tokenResult->plainTextToken,
-                    'token_type' => 'Bearer',
-                    'expires_at' => Carbon::parse(
-                        $tokenResult->accessToken->expires_at
-                    )->toDateTimeString(),
-                    'user' => [
-                        'id'    => $user->id,
-                        'name'  => $user->name,
-                        'email' => $user->email,
-                    ]
-                ]);
+        if (isset($request->email)) {
+            $user = User::where('email', $request->email)->first();
+            if ($user != null) {
+                if (Hash::check($request->password, $user->password)) {
+    
+                    $tokenResult = $user->createToken('Personal Access Token', ['server:update']);
+                    return response()->json([
+                        'result' => true,
+                        'message' => 'Đã đăng nhập thành công',
+                        'status' => 2,
+                        'access_token' => $tokenResult->plainTextToken,
+                        'token_type' => 'Bearer',
+                        'expires_at' => Carbon::parse(
+                            $tokenResult->accessToken->expires_at
+                        )->toDateTimeString(),
+                        'user' => [
+                            'id'    => $user->id,
+                            'name'  => $user->name,
+                            'email' => $user->email,
+                        ]
+                    ]);
+                } else {
+                    return response()->json(['result' => false, 'message' => 'Tài khoản hoặc mật khẩu không đúng', 'user' => null], 401);
+                }
             } else {
-                return response()->json(['result' => false, 'message' => 'Tài khoản hoặc mật khẩu không đúng', 'user' => null], 401);
+                return response()->json(['result' => false, 'message' => 'Không tìm thấy người dùng', 'status' => 0, 'user' => null], 401);
             }
-        } else {
-            return response()->json(['result' => false, 'message' => 'Không tìm thấy người dùng', 'status' => 0, 'user' => null], 401);
+        }
+        else{
+            return response()->json(['result' => false, 'message' => "OK", 'user' => null], 200);
         }
     }
 
